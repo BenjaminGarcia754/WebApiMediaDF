7587e025461a435d8ae5f12cb08758538a8fef47
Fernando Elotlan <zS20020852@estudiantes.uv.mx>
2023-12-26 21:11:06 -0600

    Se modificó el GetFavoritos pra obtener la lista

--- WebApiMediaDF/Controllers/Services/VideoServices.cs
+++ WebApiMediaDF/Controllers/Services/VideoServices.cs
@@ -15,19 +15,16 @@ namespace WebApiMediaDF.Controllers.Services
 
         public async Task<List<VideoDTO>> GetFavoritos(int id)
         {
-            List<Video> videos = new List<Video>();
-            var favoritos = _contex.VideoTypes.Where(x => x.Tipo == "Favoritos" && x.IdUsuario == id);
-            foreach (VideoType favorito in favoritos)
-            {
-                var video = await _contex.Videos.FirstOrDefaultAsync(x => x.Id == favorito.IdVideo);
-                if(video != null)
-                {
-                    videos.Add(video);
-                }
+            var videos = await (
+                from favorito in _contex.VideoTypes
+                join video in _contex.Videos on favorito.IdVideo equals video.Id
+                where favorito.Tipo == "Favoritos" && favorito.IdUsuario == id
+                select video
+            ).ToListAsync();
 
-            }
             return mapper.Map<List<VideoDTO>>(videos);
         }
+
 
         public async Task<List<VideoDTO>> GetMasTarde(int id)
         {
